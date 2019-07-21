eda6055137c9d0a64f17e8feaf6e3a23696d43fc
stahv21-meet <stahv21@meet.mit.edu>
2019-07-21 11:29:11 -0400

    Stopping here

---
+++ circle.py
@@ -0,0 +1,23 @@
+
+import turtle 
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+
+turtle.penup()
+turtle.goto(-100,0)
+turtle.pendown()
+
+turtle.goto(-95,-5)
+turtle.goto(90, -10)
+turtle.goto(85, -15)
+turtle.goto(80, -20)
+
+# ...and end it before the next line.
+turtle.mainloop() 
+# turtle.mainloop() tells the turtle to do all
+# the turtle commands above it and paint it on the screen.
+# It always has to be the last line of code!
---
+++ lab_1.py
@@ -0,0 +1,70 @@
+
+import turtle 
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+
+turtle.color("blue")
+
+turtle.goto(0,150)
+turtle.goto(10,150)
+turtle.goto(50,0)
+turtle.goto(90,150)
+turtle.goto(100,150)
+turtle.goto(100,0)
+
+turtle.penup()
+turtle.goto(125,0)
+turtle.pendown()
+
+turtle.color("dodger blue")
+
+turtle.goto(125,150)
+turtle.goto(200,150)
+turtle.penup()
+turtle.goto(125,75)
+turtle.pendown()
+turtle.goto(200,75)
+turtle.penup()
+turtle.goto(125,0)
+turtle.pendown()
+turtle.goto(200,0)
+
+turtle.penup()
+turtle.goto(225,0)
+turtle.pendown()
+
+turtle.color("deep sky blue")
+
+turtle.goto(225,150)
+turtle.goto(300,150)
+turtle.penup()
+turtle.goto(225,75)
+turtle.pendown()
+turtle.goto(300,75)
+turtle.penup()
+turtle.goto(225,0)
+turtle.pendown()
+turtle.goto(300,0)
+
+turtle.penup()
+turtle.goto(375,0)
+turtle.pendown()
+
+turtle.color("cyan")
+
+turtle.goto(375,150)
+
+turtle.penup()
+turtle.goto(325,150)
+turtle.pendown()
+turtle.goto(425,150)
+
+# ...and end it before the next line.
+turtle.mainloop() 
+# turtle.mainloop() tells the turtle to do all
+# the turtle commands above it and paint it on the screen.
+# It always has to be the last line of code!
